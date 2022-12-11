fa85475ba79bdf333826b1c7fc5604b4e6e9057b
jputnam21 <118579218+jputnam21@users.noreply.github.com>
2022-12-11 17:28:06 +1300

    Add files via upload

---
+++ research repository_2.py
@@ -0,0 +1,10 @@
+# This is some practice code 'about me'
+
+name = 'Jenny'
+print('my name is ' + name)
+gender = 'female'
+print('I am ' + gender)
+age = '51 '
+print('I am ' + age + 'years old')
+occupation = 'teacher'
+print('I am a ' + occupation)
